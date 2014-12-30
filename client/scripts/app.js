0e1fb628363fe06a3beeea2ff359cd197c5e1438
omarduarte <omarduarte@gmail.com>
2014-12-29 19:54:43 -0800

    Implemented 'Create username' and 'message send' features

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,26 +1,29 @@
 var app = {
-  server: 'https://api.parse.com/1/classes/chatterbox'
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  username: null,
+  roomname: null
 };
+
 app.init = function() {
   this.fetch();
   //window.setInterval(this.fetch.bind(app), 1000);
 };
 
 app.send = function(message) {
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
 };
 
 app.fetch = function() {
@@ -29,6 +32,7 @@ app.fetch = function() {
       // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
+    data: encodeURIComponent('order=-createdAt'),
     contentType: 'application/json',
     success: function (data) {
       console.log(data.results.length);
@@ -50,9 +54,26 @@ app.fetch = function() {
 
 $( document ).ready(function() {
   app.init();
-  $('.submit-username').on('submit', function(event){
-    alert('button working');
-  });
 
+  $('.submit-username').on('click', function(event){
+    this.username = $('.set-username').val();
+  }.bind(app));
+
+  $('.submit-message').on('click', function(event){
+    var text = $('.set-message').val();
+    var message = {
+      'username': this.username,
+      'text': text,
+      'roomname': this.roomname
+    };
+    app.send(message);
+  }.bind(app));
 });
 
+//field to type username
+  //store username in app object
+    //var username = $('.set-username').val();
+    //this.username = username;
+//field to type message
+  //store message
+//call send with newly created message object
